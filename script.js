506821fada025b23e114dd4b30517161881ecf3c
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-03-22 11:39:12 +0530

    added active navbar-links

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const head = document.querySelector("header");
 function fixednav(){
     head.classList.toggle("scrolled",window.pageYOffset>0)
 }
+
+const linsk = document.querySelectorAll(".nav-link")
+
+
 fixednav()
 window.addEventListener("load",()=>{
    
@@ -11,7 +15,10 @@ window.addEventListener("load",()=>{
 
 
 
-window.addEventListener("scroll",fixednav)
+window.addEventListener("scroll",()=>{
+    fixednav();
+    activeLink();
+})
 
 // animations -----
 let scroll = ScrollReveal({
@@ -22,3 +29,24 @@ let scroll = ScrollReveal({
 
 scroll.reveal(".hero-info", {delay : 600})
 scroll.reveal(".hero-image", { origin:"top" ,delay : 600})
+
+
+// Active link on scroll ------------------------/
+
+function activeLink(){
+    // console.log("heeh")
+    let sections = document.querySelectorAll("section[id]");
+    let passedsections = Array.from(sections).map((sct,int)=>{
+        return {
+            y:sct.getBoundingClientRect().top - head.offsetHeight,
+            id:int
+        };
+    }).filter(sct => sct.y<=0 )
+    let currSectionID = passedsections.at(-1).id;
+    // console.log(currSectionID)
+    linsk.forEach(l => l.classList.remove("active"));
+    linsk[currSectionID].classList.add("active");
+
+}
+// linsk.forEach(l => l.classList.remove("active"));
+activeLink()
